63742d40491a0338b5415730363b84f2d748058d
RidwanAdebosin <adebosinadewale@gmail.com>
2024-09-09 12:22:20 +0100

    fix:vector path

--- src/Auth/ForgotPassword.jsx
+++ src/Auth/ForgotPassword.jsx
@@ -1,37 +1,33 @@
-import React from 'react'
-import '../AuthCss/ForgetPassword.css'
-import { useNavigate } from 'react-router-dom'
-import img from '../assets/vector.svg'
-import logo from '../assets/Logo.svg'
+import CareVault from "../assets/CareVault.png";
+import "../AuthCss/ForgetPassword.css";
+import { useNavigate } from "react-router-dom";
 
 const ForgotPassword = () => {
-
-    const nav = useNavigate()
+  const nav = useNavigate();
   return (
-    <div className='forgotPassword'>
-        <div className='formDiv'>
-            <div className="logo">
-                <img src={logo} alt="" />
-                <img src={img} alt="" className='cancel'/>
-            </div>
-            <div className="form-Text">
-                <h2>Forgot Password?</h2>
-                <p>Enter your email, an email will be sent for security purpses</p>
-            </div>
-            <div className="forgotPassword-inputDiv">
-                <input type="email" placeholder='Email' />
-                <button>Send Code</button>  
-                <h5>Remember Password? <span onClick={()=>nav('/login')}>Login</span></h5>              
-            </div>
+    <div className="forgotPassword">
+      <div className="formDiv">
+        <div className="company-logo">
+          <img src={CareVault} alt="" />
+          {/* <img src={img} alt="" className="cancel" /> */}
         </div>
-     
-
-        
+        <div className="form-Text">
+          <h2>Forgot Password?</h2>
+          <p>Enter your email, an email will be sent for security purpses</p>
+        </div>
+        <div className="forgotPassword-inputDiv">
+          <input type="email" placeholder="Email" />
+          <button>Send Code</button>
+          <h5>
+            Remember Password? <span onClick={() => nav("/login")}>Login</span>
+          </h5>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ForgotPassword
+export default ForgotPassword;
 
 //    {/* <form action="" className='forgotPassword-form'>
 //             <div className='logo'>
