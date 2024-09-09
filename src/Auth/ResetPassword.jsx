9bc20e474d151a4dee84cbe205bd06cadfbb386c
RidwanAdebosin <adebosinadewale@gmail.com>
2024-09-09 12:28:00 +0100

    fix:vector path changed

--- src/Auth/ResetPassword.jsx
+++ src/Auth/ResetPassword.jsx
@@ -1,32 +1,29 @@
-import React from 'react'
-import '../AuthCss/ResetPassword.css'
-import { useNavigate } from 'react-router-dom'
-import img from '../assets/vector.svg'
-import logo from '../assets/Logo.svg'
+import "../AuthCss/ResetPassword.css";
+import { useNavigate } from "react-router-dom";
+
+import CareVault from "../../assets/CareVault.png";
 
 const ResetPassword = () => {
-
-    const nav = useNavigate()
+  const nav = useNavigate();
   return (
-    <div className='ResetPassword'>
-        <form action="" className='resetPassword-form'>
-        <div className='logo'>
-        <img src={logo} alt="" />
-        <img src={img} alt='' className='cancel'/>
-            </div>
-            <div className='form-Text'>
-                <h2>Reset Password?</h2>
-                <p>Create a new secured password</p>
-            </div>
-            <div className="resetPassword-input-div">
-                <input type="paasword"  placeholder='NewPassword'/>
-                <input type="paasword"  placeholder='Confirm NewPassword'/>
-                <button onClick={()=>nav('/successpage')}>Reset Password</button>
-            </div>
-        </form>
-
+    <div className="ResetPassword">
+      <form action="" className="resetPassword-form">
+        <div className="company-logo">
+          <img src={CareVault} alt="" />
+          {/* <img src={img} alt="" className="cancel" /> */}
+        </div>
+        <div className="form-Text">
+          <h2>Reset Password?</h2>
+          <p>Create a new secured password</p>
+        </div>
+        <div className="resetPassword-input-div">
+          <input type="paasword" placeholder="NewPassword" />
+          <input type="paasword" placeholder="Confirm NewPassword" />
+          <button onClick={() => nav("/successpage")}>Reset Password</button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ResetPassword
+export default ResetPassword;
